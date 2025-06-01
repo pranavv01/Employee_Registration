0deab4f96427923e717045e781c37470bc380593
Pranav Anand <84970839+pranavv01@users.noreply.github.com>
2025-06-01 08:36:30 +0530

    Update EmpRepo.java

--- src/main/java/com/pranav/Employee_Registration/repository/EmpRepo.java
+++ src/main/java/com/pranav/Employee_Registration/repository/EmpRepo.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface EmpRepo extends JpaRepository<Employee, Long> {
-    // No need to declare findAll()
+    
 }
