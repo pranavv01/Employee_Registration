ee04a25c24d445b7413e4902704e982a3ca4d797
pranavv01 <anand.pranav26@gmail.com>
2025-06-06 23:02:08 +0530

    updated EmpRepo

--- src/main/java/com/pranav/Employee_Registration/repository/EmpRepo.java
+++ src/main/java/com/pranav/Employee_Registration/repository/EmpRepo.java
@@ -1,11 +1,12 @@
 package com.pranav.Employee_Registration.repository;
 
 import com.pranav.Employee_Registration.model.Employee;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 @Repository
 public interface EmpRepo extends JpaRepository<Employee, Long> {
-    
+    List<Employee> findByName(String name);
 }
